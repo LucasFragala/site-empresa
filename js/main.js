f741a2ab34d5ae928fdd151e645b0ceb8f82f8c3
lucas.fragala@uniara.edu.br <lucfragala@gmail.com>
2024-02-16 12:50:02 -0300

    commit
    botao para o wpp

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ function openMenu(){
 function closeMenu(){
     ul.classList.remove('open');
 }
+
+function enviarMensagemWhatsApp() {
+    // Número de telefone para o qual você deseja enviar a mensagem
+    var numeroTelefone = "+5516992924468"; // Substitua pelo seu número de telefone com o código do país
+
+    // Mensagem que você deseja enviar
+    var mensagem = "Olá! Gostaria de entrar em contato.";
+
+    // Monta a URL do WhatsApp com o número de telefone e a mensagem
+    var url = "https://api.whatsapp.com/send?phone=" + encodeURIComponent(numeroTelefone) + "&text=" + encodeURIComponent(mensagem);
+
+    // Abre a URL no navegador
+    window.open(url);
+}
